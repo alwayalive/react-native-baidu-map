39b505931e2df4ade829b02c3fbdf7edc8eef91f
zhiangz <38268793+zhiangz@users.noreply.github.com>
2018-04-24 10:59:27 +0800

    Update NonHierarchicalDistanceBasedAlgorithm.java
    禁用 cluster 效果

--- android/src/main/java/com/baidu/mapapi/clusterutil/clustering/algo/NonHierarchicalDistanceBasedAlgorithm.java
+++ android/src/main/java/com/baidu/mapapi/clusterutil/clustering/algo/NonHierarchicalDistanceBasedAlgorithm.java
@@ -88,7 +88,7 @@ public class NonHierarchicalDistanceBasedAlgorithm<T extends ClusterItem> implem
     public Set<? extends Cluster<T>> getClusters(double zoom) {
         final int discreteZoom = (int) zoom;
 
-        final double zoomSpecificSpan = MAX_DISTANCE_AT_ZOOM / Math.pow(2, discreteZoom) / 256;
+//        final double zoomSpecificSpan = MAX_DISTANCE_AT_ZOOM / Math.pow(2, discreteZoom) / 256;
         // XXX: 将 cluster 边界范围设为 0，禁用 cluster 逻辑
         final double zoomSpecificSpan = 0;
 
